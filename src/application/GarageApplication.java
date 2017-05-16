6bac27bbc45cc74b7e664607aa8165e8247c0338
johan <johan@10.2.211.60>
2017-05-16 16:50:32 +0200

    Added new save function that eliminates the need for dialogs

--- src/application/GarageApplication.java
+++ src/application/GarageApplication.java
@@ -1,17 +1,12 @@
 
 package application;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.Locale;
 import java.util.Optional;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 /**
@@ -35,35 +30,28 @@ public class GarageApplication extends Application{
 	 * @param args
 	 */
 	public static void main(String[] args) {	
-		// launch() do the following:
-		// - creates an instance of the Main class
-		// - calls Main.init()
-		// - create and start the javaFX application thread
-		// - waits for the javaFX application to finish (close all windows)
-		// the javaFX application thread do:
-		// - calls Main.start(Stage s)
-		// - runs the event handling loop
 		Application.launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		// set default locale english 
+		Locale.setDefault(Locale.ENGLISH);
+		
 		while(login==false){
 			login();
 		}
-		if(Dialogs.confirmDialog("Start options","Launch from previous", "Would you like to launch form a previous file? If you choose not to, a new file will be created.")){
-			FileChooser chooser = new FileChooser();
-			chooser.setTitle("Choose launch file");
-			File file = chooser.showOpenDialog(new Stage());
-			open(file);
-
-		} else{
+		try{
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream("database"));
+			customerManager = (CustomerManager) in.readObject();
+			customerListView = new CustomerListView(customerManager);
+			in.close();
+		} catch (Exception e){
 			customerManager = new CustomerManager();
-		}
-		// set default locale english 
-		Locale.setDefault(Locale.ENGLISH);
+			customerListView = new CustomerListView(customerManager);
+			customerListView.save();
+		} 
 
-		customerListView = new CustomerListView(customerManager);
 		BorderPane root = new BorderPane();
 		root.setTop(new GarageMenu(customerManager, customerListView));
 		root.setCenter(customerListView);
@@ -72,7 +60,6 @@ public class GarageApplication extends Application{
 		primaryStage.setTitle("Bicycle Garage");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-
 	}
 
 	public void login(){
@@ -105,35 +92,6 @@ public class GarageApplication extends Application{
 
 	@Override
 	public void stop(){
-		// Here you can place any action to be done when the application is closed, i.e. save phone book to file.
-		if(Dialogs.confirmDialog("Save options","Save current setup before termination.", "Would you like to save the current setup to file?")){
-			FileChooser chooser = new FileChooser();
-			chooser.setTitle("Choose save directory");
-			File file = chooser.showSaveDialog(new Stage());
-			save(file);
-		}
+		customerListView.save();
 	}
-	public void save(File file) {
-		try {
-			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file));
-			out.writeObject(customerManager);
-			out.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-	}
-	
-
-	public void open(File file){
-		try{
-			ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
-			customerManager = (CustomerManager) in.readObject();
-			in.close();
-		} catch (Exception e){
-			e.printStackTrace();
-			System.exit(1);
-		}
-	}
-
 }
